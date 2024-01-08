1e1e6ba1a0ba3852d4eb75892949fdfcd3b845b5
anjeelaali <anjeelaali6@gmail.con>
2024-01-08 14:40:10 +0500

    Issue resolved.

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -4,36 +4,36 @@ import { useState, useEffect } from 'react'
 const SearchField = () => {
     const [img, setImg] = useState("");
     const [res, setRes] = useState([]);
-
-
+ 
     const fetchRequest = async () => {
         const accessKey = process.env.REACT_APP_ACCESS_KEY;
-        console.log('accessKey', accessKey)
         const data = await fetch(
-            `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${accessKey}`
+            `https://api.unsplash.com/search/photos?page=1&query=office&client_id=${accessKey}`
         );
+        console.log('data', data)
         const dataJ = await data.json();
         const result = dataJ.results;
         setRes(result);
     };
+    
+    // useEffect Hook
     useEffect(() => {
+        console.log('Useeffect is working')
         fetchRequest();
-    }, []);
+        // setImg('')
+    }, [img]);
 
     const Submit = () => {
         fetchRequest();
-        setImg("");
     };
     return (
         <>
-
             <div className='my-4 text-center '>
                 <input
                     className=' border border-gray-300 text-black outline-none text-sm rounded py-2 px-3 focus: border border-gray-50'
                     type='input'
                     placeholder='Search anything ...'
-                    value={img}
-                    onChange={(e) => setImg(e.target.value)}
+                    
 
                 />
                 <button onClick={Submit} className='border border-white mx-5 py-2 px-5 rounded focus: bg-white text-gray-900 '>
@@ -45,16 +45,14 @@ const SearchField = () => {
                 {res.map((val) => {
                     return (
                         <>
-                            <div className=' '>
+                            <div key={val.id} className=' '>
                                 <img
                                     className="col-3 img-fluid img-thumbnail"
                                     src={val.urls.small}
                                     alt="val.alt_description"
                                     width='100%'
                                 />
-
                             </div>
-
                         </>
                     );
                 })}
